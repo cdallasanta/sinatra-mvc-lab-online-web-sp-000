f2cfe88b6020c4a59770d1d55808edab75225962
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-24 23:26:32 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,8 +7,10 @@ class PigLatinizer
   def piglatinize(text)
     binding.pry
     words = text.split(" ")
-    words.each do |word|
+    words.map do |word|
       start_consonants = word.scan(/^[^aeiou]*/i)[0]
-      word.split(start_consonants)[1]
+      mid-word = word.split(start_consonants)[1]
+
+      word = mid-word + start_consonants + "ay"
   end
 end
